772614c36ae54a60e002e7e96589f1e9762f634c
Tasfia Islam <tasf1a027@gmail.com>
2020-08-23 01:07:28 +0600

    initial commit

--- library_manager/library_manager/settings.py
+++ library_manager/library_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'il_=)e9t6lflqh9xgrnn22ez&63*)&5xz$@lr8^p6zvy=!&7)3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -77,8 +78,8 @@ WSGI_APPLICATION = 'library_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': os.path.join(BASE_DIR, config('DB_NAME')),
     }
 }
 
